c7ff36ff3fac38937f7ae8189afe8a907d7e5e0e
João Pedro Barberino <joao.p.barberino@gmail.com>
2019-03-16 16:20:51 -0300

    Terminar model de livro e disponibilizar botão para download

--- app_principal/views.py
+++ app_principal/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
+from app_principal.models import Livro
 
 # Create your views here.
 
 def SOCORRO(request):
-    teste = 'teste'
-    return render(request, 'index.html', {'teste' : teste})
+    livros = Livro.objects.all()
+    
+    contexto = {
+        'livros': livros
+    }
+
+    return render(request, 'index.html', contexto)
